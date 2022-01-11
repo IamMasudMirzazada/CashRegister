790a6b8b3db0836ebb557fc209fd038827766486
IamMasudMirzazada <74949661+IamMasudMirzazada@users.noreply.github.com>
2022-01-10 23:11:44 -0800

    Cash register
    Cash register

---
+++ task Cash/Cash/Cash/CashRegister.cs
@@ -0,0 +1,62 @@
+﻿using Cash.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cash
+{
+    class CashRegister
+    {
+        public double TotalAmount { get; set; }
+        static int TotalSalesCount = 0;
+        public Currency Currency;
+
+
+        public CashRegister(int totalAmount, Currency currency)
+        {
+            TotalAmount = totalAmount;
+            Currency = currency;
+        }
+
+        public void AddNewSale(double mebleg, Currency currency)
+        {
+            if (Currency == currency)
+            {
+                TotalAmount += mebleg;
+            }
+            else
+            {
+                switch ((int)currency)
+                {
+                    case (int)Currency.dollar:
+                        TotalAmount = TotalAmount + mebleg * 1.70 ;
+                        break;
+                    case (int)Currency.manat:
+                        TotalAmount = TotalAmount + mebleg / 1.70 ;
+                        break;
+                }
+            }
+        }
+
+
+        public void RemoveSale(double mebleg, Currency currency)
+        {
+            if (Currency == currency)
+            {
+                TotalAmount -= mebleg;
+            }
+            else
+            {
+                switch ((int)currency)
+                {
+                    case (int)Currency.dollar:
+                        TotalAmount = TotalAmount - mebleg * 1.70;
+                        break;
+                    case (int)Currency.manat:
+                        TotalAmount = TotalAmount - mebleg / 1.70;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ task Cash/Cash/Cash/Program.cs
@@ -0,0 +1,20 @@
+﻿using Cash.Enums;
+using System;
+
+namespace Cash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CashRegister cr1 = new CashRegister(0, Currency.manat);
+            cr1.AddNewSale(89, Currency.dollar);
+            cr1.AddNewSale( 200, Currency.manat);
+            cr1.AddNewSale(30, Currency.manat);
+            cr1.RemoveSale(45 , Currency.dollar);
+            cr1.RemoveSale(56 , Currency.manat);
+             
+            Console.WriteLine(cr1.TotalAmount ); 
+        }
+    }
+}
